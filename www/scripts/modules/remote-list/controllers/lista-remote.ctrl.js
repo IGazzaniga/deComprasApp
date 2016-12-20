a5dd850487aae9e8f794582a87f4935f01e067be
lorenzo.soler <lorenzo@ntbflux-Lsoler>
2016-12-20 17:39:32 -0300

    se empieza funcionalidad para agregar miembros a una lista

--- www/scripts/modules/remote-list/controllers/lista-remote.ctrl.js
+++ www/scripts/modules/remote-list/controllers/lista-remote.ctrl.js
@@ -7,7 +7,25 @@ angular.module('deComprasApp.remote-list')
 	  	var listaRef = ref.child('Listas/' + $stateParams.listaRemoteId);
 	  	$scope.listaActual = $firebaseObject(listaRef);
 	  	$scope.listaActual.items = $firebaseArray(listaRef.child('items'));
+	  	$scope.allUsers = $firebaseArray(ref.child('Users'));
+	  	$scope.userSearch = {};
 
-	  	
+	  	$scope.getUsers = function() {
+	  		$scope.usersSearches = [];
+		  	angular.forEach($scope.allUsers, function(user){
+		  		if (user.name.toUpperCase().includes($scope.userSearch.name.toUpperCase()) && $scope.userSearch.name.trim() !== '' ) {
+		  			$scope.usersSearches.push(user);
+		  		}
+		  	})	  		
+		};
+
+		$scope.createModalMembers = function() {
+		  $ionicModal.fromTemplateUrl('scripts/modules/remote-list/views/modals/modalAddMember.html', {
+		    scope: $scope
+		  }).then(function(modal) {
+		    $scope.modal = modal;
+		    $scope.modal.show();
+		  });
+		};
 
 	}])
