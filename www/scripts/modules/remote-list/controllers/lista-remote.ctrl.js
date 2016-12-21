9459f894a66581cdb8278fc744f0754dafa991dc
lorenzo.soler <lorenzo@ntbflux-Lsoler>
2016-12-21 17:43:47 -0300

    funcionalidad de asignar un usuario a una lista

--- www/scripts/modules/remote-list/controllers/lista-remote.ctrl.js
+++ www/scripts/modules/remote-list/controllers/lista-remote.ctrl.js
@@ -1,22 +1,33 @@
 angular.module('deComprasApp.remote-list')
-	.controller('ListaRemoteCtrl', ['$scope', '$state', '$stateParams', '$firebaseAuth', '$firebaseObject', '$firebaseArray', '$ionicModal', 
-		function($scope, $state, $stateParams, $firebaseAuth, $firebaseObject, $firebaseArray, $ionicModal){
+	.controller('ListaRemoteCtrl', ['$scope', '$ionicSideMenuDelegate', '$ionicPopup', '$state', '$stateParams', '$firebaseAuth', '$firebaseObject', '$firebaseArray', '$ionicModal', 
+		function($scope, $ionicSideMenuDelegate, $ionicPopup, $state, $stateParams, $firebaseAuth, $firebaseObject, $firebaseArray, $ionicModal){
 	  
 	  	var ref = firebase.database().ref();
 	  	var auth = $firebaseAuth();
 	  	var listaRef = ref.child('Listas/' + $stateParams.listaRemoteId);
-	  	$scope.listaActual = $firebaseObject(listaRef);
-	  	$scope.listaActual.items = $firebaseArray(listaRef.child('items'));
-	  	$scope.allUsers = $firebaseArray(ref.child('Users'));
+	  	$scope.listaActual = $firebaseObject(listaRef)	
+	  	$scope.listaActual.$loaded().then(function(){
+	  		$scope.listaActual.users = [];
+	  	});
+  		var membersRef = listaRef.child('members');
+		$firebaseArray(membersRef).$loaded().then(function(x){
+			$scope.members = x;
+			angular.forEach($scope.members, function(user){
+				$firebaseObject(ref.child('Users/' + user.$value)).$loaded().then(function(x){
+					$scope.listaActual.users.push(x);
+				})
+			});
+		});
+	  	$firebaseArray(listaRef.child('items')).$loaded().then(function(x){
+	  		$scope.listaActual.items = x;
+	  	});
+	  	$firebaseArray(ref.child('Users')).$loaded().then(function(x){
+	  		$scope.allUsers = x;
+	  	});
 	  	$scope.userSearch = {};
 
-	  	$scope.getUsers = function() {
-	  		$scope.usersSearches = [];
-		  	angular.forEach($scope.allUsers, function(user){
-		  		if (user.name.toUpperCase().includes($scope.userSearch.name.toUpperCase()) && $scope.userSearch.name.trim() !== '' ) {
-		  			$scope.usersSearches.push(user);
-		  		}
-		  	})	  		
+	  	$scope.toggleLeft = function() {
+		  $ionicSideMenuDelegate.toggleLeft();
 		};
 
 		$scope.createModalMembers = function() {
@@ -26,6 +37,47 @@ angular.module('deComprasApp.remote-list')
 		    $scope.modal = modal;
 		    $scope.modal.show();
 		  });
+		};
+
+		$scope.addMember = function() {
+			if (!angular.equals($scope.userSearch, {}) && $scope.userSearch.name !== '') {
+				var existe = false;
+				angular.forEach($scope.members, function(userId){
+					if (userId.$value === $scope.userSearch.name.$id) {
+						existe = true;
+						$ionicPopup.alert({
+					       title: 'Atención!',
+					       template: 'El usuario seleccionado ya se encuentra como miembro en esta lista'
+					    });
+					} else {
+						if ($scope.userSearch.name.$id === undefined) {
+							existe = true;
+							$ionicPopup.alert({
+						       title: 'Atención!',
+						       template: 'El usuario seleccionado no existe en DeCompras'
+						    });
+						}
+					}
+				})
+				if (!existe) {
+					$scope.members.$add($scope.userSearch.name.$id).then(function(data){
+						$firebaseObject(ref.child('Users/' + $scope.userSearch.name.$id)).$loaded().then(function(x){
+							$scope.listaActual.users.push(x);
+						})
+						var ListsRef = ref.child('Users/' + $scope.userSearch.name.$id + '/misListas');
+						$firebaseArray(ListsRef).$loaded().then(function(x){
+							$scope.misListas = x;
+							$scope.misListas.$add($scope.listaActual.$id);
+							$scope.modal.remove();
+						});
+					});	
+				}
+			} else {
+				$ionicPopup.alert({
+			       title: 'Atención!',
+			       template: 'Seleccione un usuario para agregar'
+			     });
+			}	
 		};
 
 	}])
---
+++ www/scripts/modules/remote-list/directives/select.directive.js
@@ -0,0 +1,73 @@
+angular.module('deComprasApp.remote-list')
+    .directive('ionSelect',function(){
+        'use strict';
+        return{
+            restrict: 'EAC',
+            scope: {
+               label:'@',
+                labelText:'@',
+                labelImg: '@',
+                provider:'=',
+                ngModel: '=?',
+                ngValue: '=?',
+               
+            },
+             require: '?ngModel',
+             transclude : false,
+             replace: false,
+            template:
+                        '<div class="selectContainer">'
+                            +'<label class="item item-input item-stacked-label">' 
+                                +'<span class="input-label">{{label}}</span>'
+                                +'<div class="item item-input-inset">'
+                                    +'<label class="item-input-wrapper">'
+                                        +'<i class="icon ion-ios7-search placeholder-icon"></i>'
+                                        +'<input id="filtro" type="search"  ng-model="ngModel" ng-keydown="onKeyDown()"/>'
+                                    +'</label>'
+                                    +'<button class="button button-small button-clear" ng-click="open()">'
+                                        +'<i class="icon ion-chevron-down"></i>'
+                                    +'</button>'
+                                +'</div>' 
+                            +'</label>'
+                            +'<div class="optionList padding-left padding-right" >'
+            +'<ion-scroll>'
+                                +'<ul class="list">'
+            +'<li class="item" ng-click="selecionar(item)" ng-repeat="item in provider | filter:ngModel"><img class="foto-user" ng-src={{item[labelImg]}}>{{item[labelText]}}</li>'                    
+                                +'</ul>'
+            +'</ion-scroll>'
+                            +'</div>'    
+                        +'</div>'
+                 ,
+            link: function (scope, element, attrs,ngModel) {
+                scope.ngValue = scope.ngValue !== undefined ? scope.ngValue :'item';
+                
+                scope.selecionar = function(item){
+                    ngModel.$setViewValue(item);
+                    scope.showHide = false;
+                };
+                
+                element.bind('click',function(){
+                    element.find('input');
+                });
+                
+                scope.open = function(){
+                    
+                      scope.ngModel = "";  
+                    return scope.showHide=!scope.showHide;
+                };
+                
+                scope.onKeyDown = function(){
+                    scope.showHide = true;
+                    if(!scope.ngModel){
+                         scope.showHide = false;
+                    }
+                }
+                
+                scope.$watch('ngModel',function(newValue){
+                    if(newValue)
+               element.find('input').val(newValue[scope.labelText]);
+                   
+                });
+            },
+        };
+    });
